54dccd803e1b040ad17c976ad67e186a4f5c9045
Brianna <blr3@illinois.edu>
2020-05-19 14:02:08 -0500

    First commit

---
+++ Assets/Scripts/cubeControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeControl : MonoBehaviour
+{
+	public Material[] materials;
+    // Start is called before the first frame update
+    void Awake() 
+    {
+		Renderer rend = GetComponent<Renderer>();
+		rend.enabled = true;
+		rend.sharedMaterial = materials[Random.Range(0,(materials.Length))]; 
+	}
+    void Start()
+    {
+
+       //WaitForSeconds(5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey("right")) {
+			transform.Translate(0.1f,0,0);
+		}
+		if (Input.GetKey("left")) {
+			transform.Translate(-0.1f,0,0);
+		}
+		if (Input.GetKey("up")) {
+			transform.Translate(0,0,0.1f);
+		}
+		if (Input.GetKey("down")) {
+			transform.Translate(0,0,-0.1f);
+		}
+        
+    }
+    
+    /*
+    void OnCollisionEnter(Collision col) 
+    {
+		/*
+		foreach (ContactPoint contact in col.contacts)
+		{
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+		}
+	}
+	*/
+	
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("trigger detected");
+		Renderer rend = other.gameObject.GetComponent<Renderer>();
+		if (other.gameObject.CompareTag("Droplet"))
+		{
+			Destroy(gameObject);
+			if (rend.sharedMaterial.name == GetComponent<Renderer>().sharedMaterial.name) {
+				// add points
+			} else {
+				Destroy(gameObject);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/paintDroplets.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paintDroplets : MonoBehaviour
+{
+	public float delay = 4.5f;
+	public float spawnTime = 1.5f;
+	public GameObject paintDroplet;
+	public bool stopSpawning = false;
+	public Material[] materials;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		StartCoroutine("MakeDroplets");
+        //InvokeRepeating("Spawn", spawnTime, delay);
+    }
+
+    // Update is called once per frame
+    IEnumerator MakeDroplets()
+    {
+		/*
+		Instantiate(paintDroplet, new Vector3(Random.Range(-6,6),10,0), Quaternion.identity);
+        if (stopSpawning) {
+			CancelInvoke("SpawnObject");
+		}
+		*/
+		
+		while (true) {
+			GameObject item = Instantiate(paintDroplet, new Vector3(Random.Range(-5,5),10,Random.Range(-3,3)), Quaternion.identity);
+			Renderer rend = item.GetComponent<Renderer>();
+			rend.sharedMaterial = materials[Random.Range(0,(materials.Length))]; 
+			yield return new WaitForSeconds(.5f);
+		}
+    }
+    
+
+}
