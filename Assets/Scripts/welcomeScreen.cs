2184fa2ee687578ae3ac30cf84edf75e118d0faa
mhenriks <mhenriks@umich.edu>
2020-05-19 23:34:14 -0700

    added instructions page

--- Assets/Scripts/welcomeScreen.cs
+++ Assets/Scripts/welcomeScreen.cs
@@ -5,8 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class welcomeScreen : MonoBehaviour
 {
-	public void start() 
+	public void start()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void instructions()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
 	}
 }
